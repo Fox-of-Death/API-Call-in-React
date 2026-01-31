48ad900b6e559dd12ec8db33a188ce6ca6e60dbe
Ballny <81943754+Fox-of-Death@users.noreply.github.com>
2026-01-31 16:50:18 +0700

    Remove unnecessary blank line in Orders component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Orders = () => {
-
     return (
         <div className="container mx-auto px-4 py-8">
             <div className="bg-white rounded-lg shadow-lg p-6">
